c0b0a3edfbc1241c7b46d3df1ced5a2e7fabe413
hariknair77 <hariknair77@gmail.com>
2019-08-26 03:42:13 +0530

    bug fix

--- python/disk_update.py
+++ python/disk_update.py
@@ -1,4 +1,5 @@
 #c-scan
+
 disk = [176, 79, 34, 60, 92, 11, 41, 114]
 cp = 50
 start = 0
@@ -8,16 +9,17 @@ seq = []
 mode = 1
 # mode = int(input('Towards large(1) or small(2)'))
 if mode == 1: # towards big
-disk.sort()
-for i in range(len(disk)):
-    if disk[i] >= cp:
-        dist += (abs(cp-disk[i]) + abs(end-disk[i]))
-        seq.extend(disk[i:])
-        disk = disk[:i]
-        break
-if disk:
-    dist += abs(start-disk[0]) + abs(disk[0]-disk[-1])    
-    seq.extend(disk)
+    disk.sort()
+    for i in range(len(disk)):
+        if disk[i] >= cp:
+            dist += (abs(cp-disk[i]) + abs(disk[-1]-disk[i]))
+            temp = disk[-1]
+            seq.extend(disk[i:])
+            disk = disk[:i]
+            break
+    if disk:
+        dist += abs(end-temp) + abs(start-disk[0]) + abs(disk[0]-disk[-1])    
+        seq.extend(disk)
 # else:
 #     disk.sort(reverse=True)
 #     for i in range(len(disk)):
@@ -29,7 +31,6 @@ if disk:
 #         dist += abs(end-disk[0]) + abs(disk[0]-disk[-1])            
 
 # print(dist)    
-
 
 
 #scan
@@ -44,22 +45,22 @@ mode = 1
 disk.sort(reverse=True)
 for i in range(len(disk)):
     if disk[i] <= cp:
-        dist += (abs(cp-disk[i]) + abs(start-disk[i]))
+        dist += (abs(cp-disk[i]) + abs(disk[-1]-disk[i]))
+        temp = disk[-1]
         seq.extend(disk[i:])
         disk = disk[:i]
         break
 if disk:
     seq.append(0)
-    dist += abs(start-disk[i-1]) + abs(disk[i-1] - disk[0])  
+    dist += abs(start-temp)+abs(start-disk[i-1]) + abs(disk[i-1] - disk[0])  
     disk.reverse()
     seq.extend(disk)
     
     
-
 #fcfs disk scheduling
 # cp = 55
 disk = [55,93,176,42,148,27,14,180]
 dist = 0
 for i in range(len(disk)-1):
     dist += abs(disk[i]-disk[i+1])
-print(dist)        
+print(dist)    
